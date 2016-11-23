daf2782c6aa5c86c02e95d6f28c909548072331b
Юрій Поліщук <hdd911@mail.ru>
2016-11-23 22:45:55 +0200

    1

---
+++ TextFilesRegex/TextFilesAndWeb/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TextFilesAndWeb
+{  
+    class Program
+    {
+        public static string res = "";
+       
+
+        public static void ConsoleConfig(string title)
+        {
+            Console.Title = title;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.OutputEncoding = Encoding.GetEncoding(1251); // може буть несумісність кодувань
+            Console.InputEncoding = Encoding.GetEncoding(1251);
+        }
+    
+        public static void GetSE(string adrress)
+        {
+
+            try
+            {
+                WebRequest myRequest = WebRequest.Create(adrress);
+                WebResponse myResponse = myRequest.GetResponse();
+                //Console.Write(myResponse.ContentType);
+               
+                
+                    Stream html = myResponse.GetResponseStream();
+                    StreamReader readhtml = new StreamReader(html, Encoding.GetEncoding(1251));
+                   //StreamWriter linkswriter = new StreamWriter(@"Links.txt");// закомічано бо дужеее багато силок
+                    ///StreamWriter mailswriter = new StreamWriter(@"Mails.txt");
+                    string datahtml = readhtml.ReadToEnd();
+                    myResponse.Close();
+                    //Console.WriteLine(datahtml);
+
+                    Regex href = new Regex(@"((http)|(https)):\/\/[a-zA-Z0-9-,]+(\.[a-zA-Z0-9-,/&?=_%$#*^!]+)+");
+                    Regex email = new Regex(@"[a-zA-Z0-9-,.]+@[a-zA-Z0-9-,]+(\.[a-zA-Z0-9-,/&?=_]+)+");
+                    Match hnew = href.Match(datahtml);
+                    Match mail = email.Match(datahtml);
+
+                    while (hnew.Success)
+                    {
+                       Console.WriteLine(hnew.Value);
+                        //linkswriter.WriteLine(hnew.Value);
+                       while (mail.Success)
+                       {
+                          //mailswriter.WriteLine(hnew.Value);
+                           Console.WriteLine(mail.Value);
+                           mail = mail.NextMatch();
+                       }
+                        if (!res.Contains(hnew.Value)) { res +="\n"+hnew.Value; GetSE(hnew.Value); }
+                        hnew=hnew.NextMatch();
+
+                    }
+
+                  // mailswriter.Close();
+                   // linkswriter.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+                
+            }
+           
+
+        }
+        static void Main(string[] args)
+        {
+            ConsoleConfig("Отримання адрес");
+         GetSE(@"http://www.zu.edu.ua");
+         //Console.Write(res);
+         
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TextFilesRegex/TextFilesAndWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TextFilesAndWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TextFilesAndWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4d7f99e8-d1d5-4aee-aef0-a3363f8f7677")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
